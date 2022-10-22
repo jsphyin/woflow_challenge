64b67dc53d969da48454fe99007a2ed0881b3f17
Joseph <jsphyin@gmail.com>
2022-10-22 14:38:33 -0500

    Accounting for cycles and counting occurrence of node IDs in graph

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
-
-response = requests.get('https://nodes-on-nodes-challenge.herokuapp.com/nodes/089ef556-dfff-4ff2-9733-654645be56fe')
+import heapq
 
 ID_KEY = "id"
 CHILD_NODE_KEY = 'child_node_ids'
@@ -16,17 +15,19 @@ def is_invalid_node(node_info):
 
 def traverse_nodes(node_info):
     queue = [node_info]
-    seen = set()
+    node_dict = {}
 
     while queue:
         node = queue.pop(0)
         if is_invalid_node(node):
             print(f"Invalid node with data {node}")
             continue
-        if node[ID_KEY] in seen:
-            continue
 
-        seen.add(node[ID_KEY])
+        node_id = node[ID_KEY]
+        if node_id in node_dict:
+            node_dict[node_id] += 1
+        else:
+            node_dict[node_id] = 1
 
         if node[CHILD_NODE_KEY]:
             children_ids = ','.join(node[CHILD_NODE_KEY])
@@ -34,19 +35,34 @@ def traverse_nodes(node_info):
             child_response = request_node_info(id=children_ids).json()
 
             for child in child_response:
+                if is_invalid_node(child):
+                    print(f"Invalid child node with data {child}")
+                    continue
+                child_id = child[ID_KEY]
+                if child_id in node_dict:
+                    node_dict[child_id] += 1
+                    continue
                 queue.append(child)
-    return seen
+    return node_dict
 
+def find_most_common_node(nodes_dict):
+    heap = []
+
+    for key in nodes_dict:
+        heapq.heappush(heap, (nodes_dict[key] * -1, key))
+
+    return heap[0]
 
 def main():
     node_response = request_node_info().json()
     if len(node_response) != 1:
         print("Initial node response returned more than one node")
     initial_node_info = node_response[0]
-    print(traverse_nodes(initial_node_info))
+    nodes_dict = traverse_nodes(initial_node_info)
+    most_common_node_occurrences, most_common_node_id = find_most_common_node(nodes_dict)
+    
+    print(f"Unique node number is {len(nodes_dict.keys())}")
+    print(f"Most common node id is {most_common_node_id} with {most_common_node_occurrences * -1} occurrences")
 
-
-# Using the special variable
-# __name__
 if __name__ == "__main__":
     main()
